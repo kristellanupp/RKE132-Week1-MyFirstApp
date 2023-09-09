dcc241b371e41e663031373fc06d0b8bc5ba2af0
Kristella Nupp <kristella.nupp@tktk.ee>
2023-09-10 01:26:29 +0300

    Initial commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// This is a comment to my code
+
+//application asks for user's name
+//and greets the user
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
